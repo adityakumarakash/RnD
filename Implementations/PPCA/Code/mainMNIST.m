e4868afd045eb4fd4ba8f3c6c48ed99ef2463c6a
adityakumarakash <adityakumarakash@gmail.com>
2016-04-24 20:26:15 +0530

    Results for 5% missing data added

--- Implementations/PPCA/Code/mainMNIST.m
+++ Implementations/PPCA/Code/mainMNIST.m
@@ -73,7 +73,7 @@ d = size(imagesTrain, 1);       % observed space dimension
 % 
 
 %% Missing Data Case using EM
-missPercent = 0.01;
+missPercent = 0.05;
 fprintf(fId, 'Miss Percentage = %f\n', missPercent*100);
 MissIndex = rand(size(imagesTrain)) > 1 - missPercent;
 fprintf(fId, 'Missing values count = %d\n', sum(sum(MissIndex)));
